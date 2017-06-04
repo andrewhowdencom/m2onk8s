b08ded8137fde7e7d2b3ec26b042bbdaec255b51
Andrew Howden <hello@andrewhowden.com>
2017-06-04 13:04:14 +0200

    AD-HOC refactor (master): Improvements to the install script

--- deploy/scripts/install.sh
+++ deploy/scripts/install.sh
@@ -5,24 +5,43 @@
 #
 set -e
 
+# See sysexits.h
+EX_USAGE=64
+
 APP_ROOT="/var/www"
 
-su --shell=/bin/bash www-data --command=' \
-  php '"${APP_ROOT}/bin/magento"' setup:install \
-    --db-host="db" \
-    --db-name="magento" \
-    --db-user="magento" \
-    --db-password="thisisthemagentopassword" \
-    --db-prefix="" \
-    --admin-user="m2onk8s" \
-    --admin-password="m2onk8s" \
-    --admin-email="devnull@littleman.co" \
-    --admin-firstname="m2onk8s" \
-    --admin-lastname="developer" \
-    --base-url="http://m2onk8s.hackery.littleman.local" \
-    --language="en_GB" \
-    --timezone="UTC" && \
-  chmod "u=rx,g=,o=" /var/www/html/app/etc/'
+# Define variables
+DATABASE_HOST="${DATABASE_HOST:-db}"
+DATABASE_NAME="${DATABASE_NAME:-magento}"
+ADMIN_USER="${ADMIN_USER:-admin}"
+ADMIN_EMAIL="${ADMIN_EMAIL:-devnull@littleman.co}"
+ADMIN_FIRSTNAME="${ADMIN_FIRSTNAME:-admin}"
+ADMIN_LASTNAME="${ADMIN_LASTNAME:-user}"
+LOCALE_LANGUAGE="${LOCALE_LANGUAGE:-en_GB}"
+LOCALE_TIMEZONE="${LOCALE_TIMEZONE:-UTC}"
+
+# Check required variables
+if [[ -z "${ADMIN_PASSWORD}" ]] || [[ -z "${DATABASE_PASSWORD}" ]] || [[ -z "${BASE_URL}" ]]; then
+   echo "Missing required environment variables."
+   exit $EX_USAGE
+fi
+
+su --shell=/bin/bash www-data --command=" \
+  php '${APP_ROOT}/bin/magento' setup:install \
+    --db-host='${DATABASE_HOST}' \
+    --db-name='${DATABASE_NAME}' \
+    --db-user='${DATABASE_USER}' \
+    --db-password='${DATABASE_PASSWORD}' \
+    --db-prefix='' \
+    --admin-user='${ADMIN_USER}' \
+    --admin-password='${ADMIN_PASSWORD}' \
+    --admin-email='${ADMIN_EMAIL}'
+    --admin-firstname='${ADMIN_FIRSTNAME}' \
+    --admin-lastname='${ADMIN_LASTNAME}' \
+    --base-url='${BASE_URL}' \
+    --language='${LOCALE_LANGUAGE}' \
+    --timezone='${LOCALE_TIMEZONE}' && \
+  chmod 'u=rx,g=,o=' /var/www/html/app/etc/"
 
 # Todo: Cat the .env file, and turn it into a secret.
 
