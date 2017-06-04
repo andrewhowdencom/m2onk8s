2d5c753920780f7c7707631713c02e66bcdc0cab
Andrew Howden <hello@andrewhowden.com>
2017-06-04 13:59:16 +0200

    AD-HOC refactor (install.sh): Refactor script to run as www-data, fix permissions bug.

--- deploy/scripts/install.sh
+++ deploy/scripts/install.sh
@@ -13,6 +13,7 @@ APP_ROOT="/var/www"
 # Define variables
 DATABASE_HOST="${DATABASE_HOST:-db}"
 DATABASE_NAME="${DATABASE_NAME:-magento}"
+DATABASE_USER="${DATABASE_USER:-magento}"
 ADMIN_USER="${ADMIN_USER:-admin}"
 ADMIN_EMAIL="${ADMIN_EMAIL:-devnull@littleman.co}"
 ADMIN_FIRSTNAME="${ADMIN_FIRSTNAME:-admin}"
@@ -26,23 +27,25 @@ if [[ -z "${ADMIN_PASSWORD}" ]] || [[ -z "${DATABASE_PASSWORD}" ]] || [[ -z "${B
    exit $EX_USAGE
 fi
 
-su --shell=/bin/bash www-data --command=" \
-  php '${APP_ROOT}/bin/magento' setup:install \
-    --db-host='${DATABASE_HOST}' \
-    --db-name='${DATABASE_NAME}' \
-    --db-user='${DATABASE_USER}' \
-    --db-password='${DATABASE_PASSWORD}' \
-    --db-prefix='' \
-    --admin-user='${ADMIN_USER}' \
-    --admin-password='${ADMIN_PASSWORD}' \
-    --admin-email='${ADMIN_EMAIL}'
-    --admin-firstname='${ADMIN_FIRSTNAME}' \
-    --admin-lastname='${ADMIN_LASTNAME}' \
-    --base-url='${BASE_URL}' \
-    --language='${LOCALE_LANGUAGE}' \
-    --timezone='${LOCALE_TIMEZONE}' && \
-  chmod 'u=rx,g=,o=' /var/www/html/app/etc/"
+# Give the app/etc/ folder write permissions. This is required for Magento to add the necessary configuration to this
+# location.
+chmod 'u=rwx,g=,o' "${APP_ROOT}/app/etc/"
+
+php '${APP_ROOT}/bin/magento' setup:install \
+    --db-host="${DATABASE_HOST}" \
+    --db-name="${DATABASE_NAME}" \
+    --db-user="${DATABASE_USER}" \
+    --db-password="${DATABASE_PASSWORD}" \
+    --db-prefix="" \
+    --admin-user="${ADMIN_USER}" \
+    --admin-password="${ADMIN_PASSWORD}" \
+    --admin-email="${ADMIN_EMAIL}" \
+    --admin-firstname="${ADMIN_FIRSTNAME}" \
+    --admin-lastname="${ADMIN_LASTNAME}" \
+    --base-url="${BASE_URL}" \
+    --language="${LOCALE_LANGUAGE}" \
+    --timezone="${LOCALE_TIMEZONE}"
 
 # Todo: Cat the .env file, and turn it into a secret.
-
+# See https://stackoverflow.com/questions/30690186/how-do-i-access-the-kubernetes-api-from-within-a-pod-container
 cat "${APP_ROOT}/app/env.php"
